ca1a37ab79221c43a745525b63458cd407501fa2
Junyeong Yang <84273671+Junyeong1130@users.noreply.github.com>
2023-06-15 02:45:45 -0400

    Update APIM_simulation_MplusAutomation_code.R

--- APIM_simulation_MplusAutomation_code.R
+++ APIM_simulation_MplusAutomation_code.R
@@ -12,8 +12,10 @@ setwd("C:/Users/junye/APIM")
 createModels("C:/Users/junye/APIM/1.1.data_generation_KPARAM.txt")
 runModels("C:/Users/junye/desktop/500replication/01.apimk/01.DATA_GENERATION", recursive=TRUE)
 
-# delete the data generation input file and run below two lines
+# input file generation for parameter k method
 createModels("C:/Users/junye/APIM/1.2.KPARAM_input.txt")
+
+# run all the input files at once
 runModels("C:/Users/junye/desktop/500replication/01.apimk/01.DATA_GENERATION", recursive=TRUE)
 
 
@@ -22,11 +24,12 @@ runModels("C:/Users/junye/desktop/500replication/01.apimk/01.DATA_GENERATION", r
 createModels("C:/Users/junye/APIM/2.1.data_generation_CHIDIFF.txt")
 runModels("C:/Users/junye/desktop/500replication/02.chidiff/01.DATA_GENERATION", recursive=TRUE)
 
+# input file generation for chi-square difference test
 createModels("C:/Users/junye/APIM/2.2.CHIDIFF_input_actor_only.txt")
 createModels("C:/Users/junye/APIM/2.3.CHIDIFF_input_couple.txt")
 createModels("C:/Users/junye/APIM/2.4.CHIDIFF_input_contrast.txt")
 
-# copy and pastes the replications in the folders for each test
+# run all the input files at once
 runModels("C:/Users/junye/desktop/500replication/02.chidiff/02.1.ACTORONLY", recursive=TRUE)
 runModels("C:/Users/junye/desktop/500replication/02.chidiff/02.2.COUPLE", recursive=TRUE)
 runModels("C:/Users/junye/desktop/500replication/02.chidiff/02.3.CONTRAST", recursive=TRUE)
@@ -37,6 +40,8 @@ runModels("C:/Users/junye/desktop/500replication/02.chidiff/02.3.CONTRAST", recu
 createModels("C:/Users/junye/APIM/3.1.data_generation_MODELCONSTRAINT.txt")
 runModels("C:/Users/junye/desktop/500replication/03.modelconstraint/01.DATA_GENERATION", recursive=TRUE)
 
-# delete the data generation input file and run below two lines
+# input file generation for new-variable method
 createModels("C:/Users/junye/APIM/3.2.MODELCONSTRAINT_input_couple_contrast.txt")
+
+# run all the input files at once
 runModels("C:/Users/junye/desktop/500replication/03.modelconstraint/01.DATA_GENERATION0", recursive=TRUE)
